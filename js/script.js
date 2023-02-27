945dbf043faf71067e7fd8674523bc0cf0da2a01
guruip <82904567+guruip@users.noreply.github.com>
2023-02-27 21:59:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+"use strict"
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');   
+	});
+}
+
+
+const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
+/*----------скрипт для попапа-----------*/
+var block = document.querySelector('#idblock');
+var button= document.querySelector('#idbutton');
+button.onclick = function() {
+  if (block.style.display === 'none') {
+    block.style.display = 'flex';
+  } else {
+    block.style.display = 'none';
+  };
+  return;
+};
+
+
+var block = document.querySelector('#idblock');
+var button= document.querySelector('#closeModal');
+button.onclick = function() {
+  if (block.style.display === 'flex') {
+    block.style.display = 'none';
+	} 
+  return;
+};
+/*----------скрипт для попапа-----------*/
---
+++ sender.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+	$phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+	$to = "builder-pp@yandex.ru"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Заявка c сайта";
+	
+	$msg="
+    Имя: $name /n
+    Телефон: $phone /n
+    Почта: $email /n
+    Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $to ");
+?>
+
+<p>Привет, форма отправлена</p>
